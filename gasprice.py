8f48ce791ce37f9daf4cbabd77f04f9a8f5e4119
Akshar Dave <akshar314@gmail.com>
2022-03-21 19:30:01 -0400

    add local_filter middleware

--- gasprice.py
+++ gasprice.py
@@ -10,6 +10,7 @@ from statistics import mean
 from itertools import chain
 from web3 import Web3, HTTPProvider
 from web3.middleware import geth_poa_middleware
+from web3.middleware import local_filter_middleware
 from sanic import Sanic, response
 from retry import retry
 
@@ -20,11 +21,12 @@ WINDOW = 200
 
 
 w3 = Web3(HTTPProvider(ETH_RPC_URL))
-w3.middleware_stack.inject(geth_poa_middleware, layer=0)
+
 
 app = Sanic()
 log = logging.getLogger('sanic.error')
-
+w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+w3.middleware_onion.add(local_filter_middleware)
 app.config.LOGO = ''
 block_times = deque(maxlen=WINDOW)
 blocks_gwei = deque(maxlen=WINDOW)
